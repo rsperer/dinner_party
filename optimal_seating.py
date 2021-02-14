6a0bcd589557090111abe4688a8a2ab7e61de137
Ruth Sperer <ruth@trekace.com>
2021-02-14 07:42:06 +0200

    remove unnecessary comment

--- optimal_seating.py
+++ optimal_seating.py
@@ -17,12 +17,6 @@ NUM_TOKENS = 14
 
 
 class InputError(Exception):
-    """Exception raised for errors in the input.
-
-    Attributes:
-        expression -- input expression in which the error occurred
-        message -- explanation of the error
-    """
 
     def __init__(self, message):
         self.message = message
